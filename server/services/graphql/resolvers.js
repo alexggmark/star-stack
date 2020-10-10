83abc59c942eaa619ee361bdce4fcef74c8e4b69
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-10-10 20:15:12 +0100

    Setting up test resolver return

--- server/services/graphql/resolvers.js
+++ server/services/graphql/resolvers.js
@@ -1,7 +1,30 @@
+const posts = [
+  {
+    id: 1234567,
+    text: 'When the cows come home'
+  },
+  {
+    id: 1234568,
+    text: 'When the cows come home 2'
+  },
+  {
+    id: 1234569,
+    text: 'When the cows come home 3'
+  },
+  {
+    id: 1234560,
+    text: 'When the cows come home 4'
+  },
+  {
+    id: 1234561,
+    text: 'When the cows come home 5'
+  }
+]
+
 const resolvers = {
 	RootQuery: {
 		posts(root, args, context) {
-			return []
+			return posts
 		}
 	}
 }
