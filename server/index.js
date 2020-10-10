55b6dc72572ac26ddca259327e4be768536fe38e
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-10-10 20:00:04 +0100

    Setting up GraphQL environment - ditched yarn for npm

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from 'express'
 const app = express()
 
+import services from './services'
+
+const serviceNames = Object.keys(services)
+
+for (let i = 0; i < serviceNames.length; i++) {
+	const name = serviceNames[i]
+	if (name === 'graphql') {
+		services[name].applyMiddleware({ app })
+	} else {
+		app.use(`/${name}`, services[name])
+	}
+}
+
 app.get('*', (req, res) => {
   res.send('Hello world')
 })
